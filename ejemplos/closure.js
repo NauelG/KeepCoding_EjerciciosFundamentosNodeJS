fbecf62f507c2d4fe2cb7838b3c3133c7fa26350
NauelG <g.nauel@gmail.com>
2018-07-17 21:33:25 +0200

    Ejemplo de funcionamiento de los clousures

---
+++ ejemplos/closure.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function creaAgente(nombre) {
+    let edad = 20;
+    return {
+        getNombre: function() {
+            return nombre;
+        },
+        getEdad: function() {
+            return edad;
+        },
+        saluda: function() {
+            console.log(`Hola soy ${nombre} y tengo ${edad} años`)
+        }
+    }
+}
+
+const jones = creaAgente('Jones');
+
+jones.saluda();
+
+const brown = creaAgente('Brown')
+
+brown.saluda();
+
+setTimeout(jones.saluda, 2000);
