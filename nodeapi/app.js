36dbacf1d5a2a8473e7b2e5f0beb3d1d4e4a0429
NauelG <g.nauel@gmail.com>
2018-07-20 00:27:45 +0200

    Se añaden mas rutas con middlewares y se cambia el motor de plantillas a html

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -8,14 +8,18 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Variables globales de template
+app.locals.titulo = 'NodeApi'
 
 // Rutas de mi app web
 app.use('/', require('./routes/index'));
@@ -28,6 +32,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+    // Error de validación
+    if (err.array) {
+        err.status = 422;
+        const errorInfo = err.array({ onlyFirstError: true })[0];
+        err.message = `Not valid - ${errorInfo.param} ${errorInfo.msg}`;
+    }
+
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const { query, validationResult } = require('express-validator/check');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // La vista se renderiza EN EL SERVIDOR
-    res.render('index', { title: 'Express' });
+
+    const segundo = new Date().getSeconds();
+
+
+    res.locals.valor = '<script>alert("HolaMundo")</script>';
+    res.render('index', {
+        condicion: {
+            segundo, // Sería lo mismo que poner segundo:segundo
+            par: segundo % 2 === 0
+        },
+        users: [
+            { name: 'Smith', age: 20 },
+            { name: 'Jones', age: 18 },
+            { name: 'Thomas', age: 21 }
+        ]
+    });
 });
 
 router.get('/otrapagina', function(req, res, next) {
@@ -27,6 +44,20 @@ router.get('/params/:id([0-9]+)/piso/:piso/puerta/:puerta', (req, res, next) => 
     const id = req.params.id;
     console.log(req.params)
     res.send('Ok, recibido dato opcional:' + id);
+});
+
+router.get('/query', [
+    query('talla').isNumeric().withMessage('Debe ser un número')
+], (req, res, next) => {
+    // Los parametros pasados por query string no se tienen que definir en la ruta
+    console.log(req.query);
+    validationResult(req).throw(); // Este metodo pasa los errores de validación a next(err)
+    res.send('Ok, recibidos los datos');
+});
+
+router.post('/body', (req, res, next) => {
+    console.log('req.body', req.body);
+    res.send('Ok, recibido en el body');
 });
 
 module.exports = router;
